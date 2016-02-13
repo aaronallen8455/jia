35bc6683644d1778915e8dde01ac6b21b29c69f0
Aaron Allen <aaronallen8455@gmail.com>
2016-02-12 22:16:17 -0600

    Row Height fixed

--- js/calendar.js
+++ js/calendar.js
@@ -146,6 +146,7 @@ window.onload = function() {
                     var odd = i%2 === 1;
                     new DayDisplay(year, month, _date+i, odd, result);
                 }
+                resizeEventElements();
             }
         }
         req.send(msg);
@@ -319,6 +320,39 @@ window.onload = function() {
         daySelectHandler(false, lastSelected[0], lastSelected[1], lastSelected[2]);
     };
     //getting the number of days in a month
+    
+    
+    window.addEventListener('resize', resizeEventElements, false);
+    //Event element sizer. Make row heights uniform
+    function resizeEventElements() {
+        var elements = document.getElementsByClassName('eventDivWrapper');
+        var margin = parseInt(window.getComputedStyle(elements[0].children[0]).marginTop.slice(0,-2))*2;
+        //determine how many items are in each row
+        var numPerRow = 1;
+        var ele = elements[0];
+        while (ele.nextElementSibling.offsetTop === ele.offsetTop) {
+            numPerRow++;
+            ele = ele.nextElementSibling;
+        }
+        Array.prototype.forEach.call(elements, function(x){x.style.height = null;}); //disable the artificial height.
+        if (numPerRow === 1) return; //exit if single column
+        //set the height of each element to greatest in row
+        for (var i=0; i<elements.length; i+=numPerRow) {
+            var row = [elements[i]];
+            //isolate the row
+            while (row.length < numPerRow && elements[i+row.length]) {
+                row.push(elements[i+row.length]);
+            }
+            //determine the greatest height
+            row.sort(function(a,b){return b.offsetHeight - a.offsetHeight;});
+            var height = row[0].offsetHeight - margin;
+            //set heights equal
+            row.forEach(function(x,t,a){
+                if (t === 0) return;
+                a[t].style.height = parseFloat(height) + 'px';
+            });
+        }
+    }
     
     function numDays(year, month) {
         var leapYear = (year%4 === 0);
