a1536ad7cdb0ab27a404d49bce587365c8103f65
Aaron Allen <aaronallen8455@gmail.com>
2016-03-10 04:06:31 -0600

    redid bgscroll again

--- js/bgscroll.js
+++ js/bgscroll.js
@@ -1,23 +1,40 @@
 window.addEventListener('load', function() {
     //get the elements with bg images
-    var header = document.querySelector('.splashWide');
+    //var header = document.querySelector('.splashWide');
     var headerNarrow = document.querySelector('.splashNarrow');
     var calHeader = document.querySelector('.calHeader');
+    var useNarrow = window.getComputedStyle(headerNarrow).display !== 'none';
 
-    //get the bg image height/width ratio
-    function getSizeRatio(ele) {
-        var css = window.getComputedStyle(ele).backgroundImage;
-        var url = css.slice(5,-2);
-        var image = new Image();
-        image.src = url;
-        return image.height/image.width;
+    var bgSlideWide = document.getElementById('bgSlideWide');
+    var bgSlideNarrow = document.getElementById('bgSlideNarrow');
+    var bgSlideCal = document.getElementById('bgSlideCal');
+    defineImgHeight(bgSlideWide);
+    defineImgHeight(bgSlideNarrow);
+    defineImgHeight(bgSlideCal);
+
+    function bgSlideHandler(ele, scrollY) {
+        //get element center
+        if (ele._top > halfWay*2+scrollY) return;
+        var winCenter = scrollY + halfWay;
+        ele.style.top = ((winCenter - ele.center)* .2 - ele.heightDiff)  + 'px';
     }
-    //var headerRatio = getSizeRatio(header);
-    //var calHeaderRatio = getSizeRatio(calHeader);
-    //var headerNarrowRatio = getSizeRatio(headerNarrow);
-    defineImgHeight(header);
-    defineImgHeight(calHeader);
-    defineImgHeight(headerNarrow);
+    window.addEventListener('scroll', function() {
+        window.requestAnimationFrame(function(){
+            var scrollY = window.scrollY;
+            if (!useNarrow)
+                bgSlideHandler(bgSlideWide, scrollY);
+            else
+                bgSlideHandler(bgSlideNarrow, scrollY);
+            bgSlideHandler(bgSlideCal, scrollY);
+        });
+
+    });
+
+
+
+    //defineImgHeight(header);
+    //defineImgHeight(calHeader);
+    //defineImgHeight(headerNarrow);
 
     var halfWay = window.innerHeight/2;
 
@@ -27,12 +44,21 @@ window.addEventListener('load', function() {
         var url = css.slice(5,-2);
         var image = new Image();
         image.src = url;
-        ele.imgHeight = (image.height - ele.offsetHeight)/2;
+        //ele.imgHeight = (image.height - ele.offsetHeight)/2;
+        ele.imgHeight = image.height/2;
+        //make element same size as image
+        ele.style.height = image.height + 'px';
         image = null;
         //get document coord for top
         ele._top = getTop(ele);
         //half of height
         ele.halfHeight = ele.offsetHeight/2;
+        //container height
+        ele.containerHeight = ele.parentElement.offsetHeight/2;
+        //image height - container height
+        ele.heightDiff = ele.imgHeight - ele.containerHeight;
+        //center
+        ele.center = ele._top + ele.containerHeight;
     }
 
     function getTop(ele) {
@@ -43,41 +69,57 @@ window.addEventListener('load', function() {
         }
     }
 
-    function scrollHandler(element, halfWay) {
-        var top = element._top - window.scrollY;
-        //console.log(top);
+    /*function scrollHandler(element, halfWay, scrollY) {
+        var top = element._top - scrollY;
         if (top > halfWay*2) return;
         var eleCenter = element.halfHeight + top;
-        //image is centered when at top - height/4
-        //var center = (element.imgHeight - element.offsetHeight)/2;
-        //element.style.backgroundPosition = '50% ' + (eleCenter-element.imgHeight + (halfWay-eleCenter) *rate) + 'px';
         var offset = top - element.imgHeight + (halfWay-eleCenter)*rate;
         return (element.style.backgroundPosition = '50% ' + offset + 'px');
     }
-//1050 * x = .25& 500 * x = .9
     var rate = -1.25/450 * screen.availHeight + 3.1667;//screen.availHeight/4200;
-    
-    scrollHandler(header, halfWay);
-    scrollHandler(calHeader, halfWay);
-    scrollHandler(headerNarrow, halfWay);
-    
+
+    var scrollY = window.scrollY;
+    scrollHandler(header, halfWay, scrollY);
+    scrollHandler(calHeader, halfWay, scrollY);
+    scrollHandler(headerNarrow, halfWay, scrollY);
+
+
     window.onscroll = function() {
         window.requestAnimationFrame(function() {
-            scrollHandler(header, halfWay);
-            scrollHandler(calHeader, halfWay);
-            scrollHandler(headerNarrow, halfWay);
+            //only get the scrollY once for all three
+            var scrollY = window.scrollY;
+            scrollHandler(calHeader, halfWay, scrollY);
+            if (!useNarrow)
+                scrollHandler(header, halfWay, scrollY);
+            else
+                scrollHandler(headerNarrow, halfWay, scrollY);
         });
-    };
+    };*/
     //allow window size to change without weird behavior
     window.addEventListener('resize', function() {
-        scrollHandler(header, halfWay);
-        scrollHandler(calHeader, halfWay);
-        scrollHandler(headerNarrow, halfWay);
+        var scrollY = window.scrollY;
+        //scrollHandler(header, halfWay, scrollY);
+        //scrollHandler(calHeader, halfWay, scrollY);
+        //scrollHandler(headerNarrow, halfWay, scrollY);
         //re-calc img measurement
-        defineImgHeight(header);
-        defineImgHeight(calHeader);
-        defineImgHeight(headerNarrow);
+        //defineImgHeight(header);
+        //defineImgHeight(calHeader);
+        //defineImgHeight(headerNarrow);
+        defineImgHeight(bgSlideWide);
+        defineImgHeight(bgSlideNarrow);
+        defineImgHeight(bgSlideCal);
+
+        bgSlideHandler(bgSlideWide, scrollY);
+        bgSlideHandler(bgSlideNarrow, scrollY);
+        bgSlideHandler(bgSlideCal, scrollY);
+
         //re-calc window height
         halfWay = window.innerHeight/2;
+        //see if narrow is being used
+        useNarrow = window.getComputedStyle(headerNarrow).display !== 'none';
     }, false);
+
+    bgSlideHandler(bgSlideWide, scrollY);
+    bgSlideHandler(bgSlideNarrow, scrollY);
+    bgSlideHandler(bgSlideCal, scrollY);
 });
